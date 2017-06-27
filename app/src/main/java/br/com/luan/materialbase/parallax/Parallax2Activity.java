81424660a56d3993404b419702f7fd17ad2e83b4
Luan Silva <luan@squarebits.com.br>
2017-06-27 17:51:58 -0300

    parallax
    parallax effect

---
+++ app/src/main/java/br/com/luan/materialbase/parallax/Parallax2Activity.java
@@ -0,0 +1,15 @@
+package br.com.luan.materialbase.parallax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.luan.materialbase.R;
+
+public class Parallax2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parallax2);
+    }
+}
